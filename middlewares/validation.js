71dff9c9e448ed6e8856c4ad01f298d8d14cc445
Fernando Lopez <cainuriel@gmail.com>
2024-05-20 00:12:18 +0200

    fixed error

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,7 +4,7 @@ const validation = (req, res, next) => {
 
     const err = validationResult(req);
     console.log(`err`, err);
-    if(err.isEmpty) {
+    if(!err.isEmpty()) {
         return res.status(400).json(
             {ok: false,
             errors: err.mapped()}
