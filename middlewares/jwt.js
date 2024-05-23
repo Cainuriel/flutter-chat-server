6225b80eb9ab3f6cc0cddea2649743a79def3e8f
Fernando Lopez <cainuriel@gmail.com>
2024-05-23 18:40:01 +0200

    socket auth token

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -17,6 +17,21 @@ const validationJWT = (req, res, next) => {
      
 }
 
+const checkJWT = (token = "") =>  {
+
+    try {
+
+        const { uid } = jwt.verify(token, process.env.JWT_KEY);
+        return [true, uid];
+    } catch (error) {
+        console.log(`error validationJWT in socket`, error );
+        return [false, "invalid token"];
+    }
+
+}
+
+
 module.exports = {
-    validationJWT
+    validationJWT,
+    checkJWT
 }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,18 @@
 const { io } = require('../index');
+const { checkJWT } = require('../middlewares/jwt');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    // console.log(`client.handshake.headers x-token: `, client.handshake.headers['x-token']);
+    const [isValid, uid] = checkJWT(client.handshake.headers['x-token']);
+    // console.log(`isValid, uid`, isValid, uid);
+
+    if(!isValid) return client.disconnect();
+
+    console.log(`socket autenticó cliente`);
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -18,3 +27,5 @@ io.on('connection', client => {
 
 
 });
+
+
