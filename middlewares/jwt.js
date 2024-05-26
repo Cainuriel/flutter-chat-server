5cfcc35362d607cb739f45a426986a13963740a7
Yuri <ygonzoo@gmail.com>
2024-05-26 03:52:04 +0200

    change x-token with autorization header

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,37 +1,34 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+// middleware
 const validationJWT = (req, res, next) => {
+  // const token = req.header('x-token');
+  const token = req.header("Authorization")?.split(" ")[1];
 
-    const token = req.header('x-token');
-    if(!token) return res.status(401).json({ok: false, msg: "No token"});
+  if (!token) return res.status(401).json({ ok: false, msg: "No token" });
 
-    try {
+  try {
+    const { uid } = jwt.verify(token, process.env.JWT_KEY);
+    req.uid = uid; // necesitamos la uid en el request
+    next();
+  } catch (error) {
+    console.log(`error validationJWT`, error);
+    return res.status(401).json({ ok: false, msg: "Invalid token" });
+  }
+};
 
-        const { uid } = jwt.verify(token, process.env.JWT_KEY);
-        req.uid = uid; // necesitamos la uid en el request
-        next(); 
-    } catch (error) {
-        console.log(`error validationJWT`, error );
-        return res.status(401).json({ok: false, msg: "Invalid token"});
-    }
-     
-}
-
-const checkJWT = (token = "") =>  {
-
-    try {
-
-        const { uid } = jwt.verify(token, process.env.JWT_KEY);
-        return [true, uid];
-    } catch (error) {
-        console.log(`error validationJWT in socket`, error );
-        return [false, "invalid token"];
-    }
-
-}
-
+// function
+const checkJWT = (token = "") => {
+  try {
+    const { uid } = jwt.verify(token, process.env.JWT_KEY);
+    return [true, uid];
+  } catch (error) {
+    console.log(`error validationJWT in socket`, error);
+    return [false, "invalid token"];
+  }
+};
 
 module.exports = {
-    validationJWT,
-    checkJWT
-}
+  validationJWT,
+  checkJWT,
+};
